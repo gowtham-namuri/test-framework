2c449faa2a7a90ba6dfa72d344663cf4044ac356
Gowtham Namuri <gowtham.namuri@tarams.com>
2019-04-09 12:26:24 +0530

    Initial Commit

---
+++ TestFramework2/TestFramework2.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework2.h
+//  TestFramework2
+//
+//  Created by Gowtham on 09/04/19.
+//  Copyright © 2019 Gowtham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework2.
+FOUNDATION_EXPORT double TestFramework2VersionNumber;
+
+//! Project version string for TestFramework2.
+FOUNDATION_EXPORT const unsigned char TestFramework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework2/PublicHeader.h>
+
+
